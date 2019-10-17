da4367aa4c6cf9b027a35a24710b724cbcd6782f
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 19:54:07 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,9 @@ def hopper
         }
      }
 
-
+programmer_hash.delete(:alan_kay)
+programmer_hash.delete(:dennis_ritchie)
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,13 +58,14 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#:yukihiro_matsumoto => {
+ # :known_for => "Ruby",
+#   :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
@@ -79,8 +83,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] ="LISP", "C"
 
-    
+    programmer_hash
 end
 
 def changing_alan
@@ -99,9 +107,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+
+
+   programmer_hash[:alan_kay][:known_for] = "GUI"
+   programmer_hash
+  # alans_new_info = "GUI"
      
 end
 
@@ -121,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = "Smalltalk", "Assembly" "LISP"
+programmer_hash
 end
